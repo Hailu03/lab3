85f95422acc8924903b923d35745bb495beffd9b
Hailu03 <10421016@student.vgu.edu.vn>
2023-05-17 22:06:12 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+import sklearn
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# load dataset
+diabetes = load_diabetes()
+
+# describe dataset
+print(diabetes.DESCR)
+
+# divide dataset into features and targets
+features = diabetes.data
+targets = diabetes.target
+
+# divide dataset into training and testing sets
+features_train, features_test, targets_train, targets_test = train_test_split(features, targets, test_size=0.2)
+
+# create model
+model = LinearRegression()
+model.fit(features_train, targets_train)
+pred = model.predict(features_test)
+
+# calculate mean squared error
+print("Error of the Linear Regresson: ", mean_squared_error(targets_test, pred))
