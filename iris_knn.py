6eb679fb7530f93e2cb0004cd8729eb71c557efd
Hailu03 <10421016@student.vgu.edu.vn>
2023-05-19 17:53:14 +0700

    Quang Vu Dep Zai

--- iris_knn.py
+++ iris_knn.py
@@ -1,24 +1,38 @@
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 # load dataset
 iris = load_iris()
+
 # describe dataset
 print(iris.DESCR)
+
 # Filter the data for two specific classes
 X = iris.data
-y = iris.target  # Fix: Assign iris.target to y instead of iris
+y = iris.target 
+
+# shuffle the dataset
+randNum = np.arange(X.shape[0])
+np.random.shuffle(randNum)
+X = X[randNum]
+y = y[randNum]
 
 # divide dataset into training and testing sets with a ratio of 80/20
 features_train, features_test, targets_train, targets_test = train_test_split(X,y, test_size=0.2)
 
 # create model
-model = LogisticRegression()
-model.fit(features_train, targets_train)
-prediction = model.predict(features_test)
+n_neighbors = 3
+knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+# Train the model using the training set
+knn.fit(features_train, targets_train)
+
+# predict the class labels for the test set
+prediction = knn.predict(features_test)
+
 # calculate the accuracy of the trained model on the testing set
-accuracy = accuracy_score(targets_test, prediction)  # Fix: Use targets_test instead of targets_train
-print("Accuracy of the Logistic Regression:", accuracy)
+accuracy = accuracy_score(targets_test, prediction)
+print(f"Accuracy of the KNN Classifier: {accuracy*100}%")
--- iris_logistic_regression.py
+++ iris_logistic_regression.py
@@ -4,18 +4,33 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
+
 # load dataset
 iris = load_iris()
+
 # describe dataset
 print(iris.DESCR)
+
 # Filter the data for two specific classes
 X = iris.data
-y = iris.target  # Fix: Assign iris.target to y instead of iris
+y = iris.target  
 
-class1_label = 0
-class2_label = 1
+# shuffle the dataset
+randNum = np.arange(X.shape[0])
+np.random.shuffle(randNum)
+X = X[randNum]
+y = y[randNum]
+
+# choose two classes randomly
+class1_label = np.random.randint(0, 3)
+while True:
+    class2_label = np.random.randint(0, 3)
+    if class2_label != class1_label:
+        break
+
 class1_samples = X[y == class1_label]
 class2_samples = X[y == class2_label]
+
 # Combine the samples from the two classes
 filtered_samples = np.concatenate((class1_samples, class2_samples), axis=0)
 filtered_labels = np.concatenate((np.zeros(len(class1_samples)), np.ones(len(class2_samples))), axis=0)
@@ -23,21 +38,13 @@ filtered_labels = np.concatenate((np.zeros(len(class1_samples)), np.ones(len(cla
 # divide dataset into training and testing sets with a ratio of 80/20
 features_train, features_test, targets_train, targets_test = train_test_split(filtered_samples, filtered_labels, test_size=0.2)
 
-# create model
+# create logistic regression model
 model = LogisticRegression()
 model.fit(features_train, targets_train)
+
+# predict the class labels for the test set
 prediction = model.predict(features_test)
 
-# Train a k-NN model (using Sklearn) with different hyperparameters on the training set.
-#  define values for different hyperparameters
-n_neighbors_values = [5, 3, 7]
-for n_neighbors in n_neighbors_values:
-    #  K -NN model (SK learn ) on the training set
-    knn = KNeighborsClassifier(n_neighbors=3)
-
-    # Train the model using the training set
-    knn.fit(features_train, targets_train)
-
-    # calculate the accuracy of the trained model on the testing set
-    accuracy = accuracy_score(targets_test, prediction)  # Fix: Use targets_test instead of targets_train
-    print("Accuracy of the Logistic Regression:", accuracy)
+# calculate the accuracy of the trained model on the testing set
+accuracy = accuracy_score(targets_test, prediction)
+print(f"Accuracy of the Logistic Regression: {accuracy*100}%")
